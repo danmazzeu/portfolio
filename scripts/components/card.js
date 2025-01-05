78ff457020ddf22588e4487c1d204d51fa8fb020
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-05 17:24:55 -0300

    Update

--- scripts/components/card.js
+++ scripts/components/card.js
@@ -1,14 +1,22 @@
 $(document).ready(function() {
+    let delay = 0;
+
     $(window).scroll(function() {
         $('.card, .card-inverse, img').each(function() {
             var $this = $(this);
             var viewportCenter = $(window).scrollTop() + $(window).height() / 3;
 
             if (viewportCenter >= $this.offset().top && viewportCenter <= $this.offset().top + $this.outerHeight()) {
-                $this.addClass('active');
+                setTimeout(function() {
+                    $this.addClass('active');
+                }, delay);
+
+                delay += 150;
             } else {
                 $this.removeClass('active');
             }
         });
+
+        delay = 0; 
     });
 });
