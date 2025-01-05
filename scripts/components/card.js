d10fcaa7961c14ada724df1350b62db1f313fe48
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-05 17:47:30 -0300

    Update

--- scripts/components/card.js
+++ scripts/components/card.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
     $(window).scroll(function() {
         $('.card, .card-inverse, img').each(function() {
             var $this = $(this);
-            var viewportCenter = $(window).scrollTop() + $(window).height() / 3;
+            var viewportTop = $(window).scrollTop();
+            var viewportBottom = viewportTop + $(window).height(); 
 
-            if (viewportCenter >= $this.offset().top && viewportCenter <= $this.offset().top + $this.outerHeight()) {
+            if (viewportBottom >= $this.offset().top && viewportTop <= $this.offset().top + $this.outerHeight()) {
                 setTimeout(function() {
                     $this.addClass('active');
                 }, delay);
