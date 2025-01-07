93f5126e70452b1214cdbac1639600dd8a18d168
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-07 08:57:41 -0300

    update

---
+++ scripts/global/themes.js
@@ -0,0 +1,25 @@
+const themes = [
+    '#ffff00', // Yellow
+    '#bad411', // Lime
+    '#159fc2', // Blue
+    '#ebaa50', // Light Orange
+    '#00ffff', // Light Blue
+];
+  
+  let previousTheme = null;
+  
+function setRandomTheme() {
+    let randomIndex;
+    
+    do {
+        randomIndex = Math.floor(Math.random() * themes.length);
+    } while (randomIndex === previousTheme);
+  
+    const randomTheme = themes[randomIndex];
+    document.documentElement.style.setProperty('--theme', randomTheme);
+    previousTheme = randomIndex;
+}
+  
+window.onload = setRandomTheme;
+  
+setInterval(setRandomTheme, 10000);
