2b075aa98e652a604f2c5c3f48da1068f5fb14e1
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-11 13:10:56 -0300

    update

--- scripts/components/animation.js
+++ scripts/components/animation.js
@@ -2,7 +2,7 @@ $(document).ready(function() {
     let delay = 0;
 
     $(window).scroll(function() {
-        $('.card, video').each(function() {
+        $('.card, .presentation, video').each(function() {
             var $this = $(this);
             var viewportTop = $(window).scrollTop() + $(window).height() / 2 - $this.outerHeight();
             var viewportBottom = viewportTop + $this.outerHeight(); 
