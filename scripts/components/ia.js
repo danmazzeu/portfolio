e575c7d8ac9a311a73632070b0a28ba08d232e68
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-11 17:25:38 -0300

    update

--- scripts/components/ia.js
+++ scripts/components/ia.js
@@ -11,7 +11,7 @@ $(document).ready(function() {
         if (apiAnswer.includes('enigma')) {
             requestData = {
                 contents: [{
-                    parts: [{ text: 'Repita exatamente a seguinte frase: Existe um arquivo html na raiz, cujo nome é tão óbvio quanto o próprio local, lá você encontrará os próximos passos.' }]
+                    parts: [{ text: 'Repita exatamente a seguinte frase: Existe um arquivo html na raiz, cujo nome você ouvira em algum lugar desta página, lá você encontrará os próximos passos.' }]
                 }]
             };
           } else {
