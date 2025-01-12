807b5203081cc0fea84cb081f687a432850a043a
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-12 12:26:24 -0300

    update

--- scripts/components/animation.js
+++ scripts/components/animation.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
     let delay = 0;
+    let audioPlayed = false; 
 
     $(window).scroll(function() {
+        const audio = $('audio#scrollbarAudio').length ? $('audio#scrollbarAudio')[0] : new Audio('/audios/scrollbar.mp3');
+        audio.volume = 0.5;
+
+        if (!audio.id) {
+            audio.id = 'scrollbarAudio';
+        }
+
+        if ($(window).scrollTop() > 0 && !audioPlayed) {
+            audio.play();
+            audioPlayed = true;
+        }
+
         $('.card, .feedcard, .presentation, video').each(function() {
             var $this = $(this);
             var viewportTop = $(window).scrollTop() + $(window).height() / 2 - $this.outerHeight();
@@ -19,5 +32,17 @@ $(document).ready(function() {
         });
 
         delay = 0; 
+    });
+
+    $('.card, .feedcard, .gamecard, .presentation, video').each(function() {
+        const audio = new Audio('/audios/cards.mp3');
+        audio.volume = 0.3;
+
+        $(this).mouseover(function() {
+            audio.play();
+        }).click(function() {
+            audio.currentTime = 0;
+            audio.play();
+        });
     });
 });
