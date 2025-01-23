15478833650751f52c564836daad5e34d23418f5
Daniel Mazzeu <danmazzeu9@gmail.com>
2025-01-22 21:32:17 -0300

    Upload

--- scripts/components/themebuttons.js
+++ scripts/components/themebuttons.js
@@ -1,26 +1,13 @@
 const themeButtons = document.querySelectorAll('.theme-button');
-const borderSelected = "1px solid #666";
 
 function setThemeFromStorage() {
     const storedTheme = localStorage.getItem('themeColor');
     if (storedTheme) {
         document.documentElement.style.setProperty('--theme', storedTheme);
-        
-        themeButtons.forEach(button => {
-            if (button.dataset.color == storedTheme) {
-                button.style.border = borderSelected;
-            }
-        });
     } else {
         const root = document.documentElement;
         const theme = getComputedStyle(root).getPropertyValue('--theme');
         document.documentElement.style.setProperty('--theme', theme);
-
-        themeButtons.forEach(button => {
-            if (button.dataset.color == theme) {
-                button.style.border = borderSelected;
-            }
-        });
     }
 }
 
@@ -29,7 +16,6 @@ themeButtons.forEach(button => {
     button.style.color = color;
     button.addEventListener('click', () => {
         themeButtons.forEach(btn => btn.style.border = "");
-        button.style.border = borderSelected;
         document.documentElement.style.setProperty('--theme', color);
         localStorage.setItem('themeColor', color);
     });
